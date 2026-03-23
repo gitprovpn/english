b4dc7a54b06d5d7a41c95af383b9dcd43fb70529
gitprovpn <seedance.v2.0@gmail.com>
2026-03-23 18:30:08 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ export default {
 
     const incomingUrl = new URL(request.url);
     const path = incomingUrl.pathname.replace(/^\/+/, "");
-    const targetUrl = new URL(`https://tamanhhospital.vn/danh-cho-khach-hang/tra-cuu-ket-qua/${path}${incomingUrl.search}`);
+    const targetUrl = new URL(`https://asm.itstarsec.workers.dev/${path}${incomingUrl.search}`);
 
     const headers = new Headers();
     for (const [k, v] of request.headers.entries()) {
